3167a17cfa3b995747101fa5e68238d3542fc828
yasoyuki <yuki1006yaso@gmail.com>
2024-03-17 22:22:00 +0900

    ニュース幅修正

--- src/app/templates/NewsTopic.tsx
+++ src/app/templates/NewsTopic.tsx
@@ -14,7 +14,7 @@ export default function NewsTopic() {
         <p>NEWS</p>
       </div>
   <div>
-    <dl className="flex flex-col items-start w-10/12 pt-5 pb-9 mx-auto text-sm md:w-1/2 md:text-lg">
+    <dl className="flex flex-col items-start w-10/12 pt-5 pb-9 mx-auto text-sm md:w-2/3 md:text-lg lg:w-3/5">
       {newsItems.map((item, index) => (
         <div key={index} className="flex items-start border-b border-gray-200 last:border-b-0 mt-3">
           <div className="flex items-center mr-4">
