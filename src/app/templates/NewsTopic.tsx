13dbde289fa91c70820742388248c9f9407640c3
yasoyuki <yuki1006yaso@gmail.com>
2024-03-24 13:51:20 +0900

    最終修正

--- src/app/templates/NewsTopic.tsx
+++ src/app/templates/NewsTopic.tsx
@@ -2,9 +2,9 @@ import "../globals.css";
 
 
 const newsItems = [
-  { date: "2024.02.21", content: "最新のニュース最新のニュース最新のニュース最新のニュース" },
-  { date: "2024.01.30", content: "2番目のニュース2番目のニュース2番目のニュース2番目のニュース" },
-  { date: "2024.01.01", content: "3番目のニュース3番目のニュース3番目のニュース" },
+  { date: "2024.02.21", content: "3~4名様用スペースの料金を改定いたしました。詳しくはご予約ページを参照ください" },
+  { date: "2024.01.30", content: "シブヤサンドイウィッチとのコラボメニューを販売" },
+  { date: "2024.01.01", content: "お得な月額プランがスタートします！個人・法人どなたでもご利用いただけるお得なプランです" },
 ];
 
 export default function NewsTopic() {
@@ -14,13 +14,13 @@ export default function NewsTopic() {
         <p>NEWS</p>
       </div>
   <div>
-    <dl className="flex flex-col items-start w-10/12 pt-5 pb-9 mx-auto text-sm md:w-2/3 md:text-lg lg:w-3/5">
+    <dl className="flex flex-col items-start w-10/12 pt-5 pb-9 mx-auto text-sm md:w-2/3 md:text-base lg:w-3/5">
       {newsItems.map((item, index) => (
         <div key={index} className="flex items-start border-b border-gray-200 last:border-b-0 mt-3">
           <div className="flex items-center mr-4">
             {index === 0 ? <p className="text-red-600">new!</p> : <p className="opacity-0">new!</p>}
           </div>
-          <dt className="mr-2">{item.date}</dt>
+          <dt className="mr-6">{item.date}</dt>
           <hr />
           <dd>{item.content}</dd>
         </div>
--- src/app/templates/SpaceType.tsx
+++ src/app/templates/SpaceType.tsx
@@ -11,22 +11,22 @@ export default function SpaceType() {
                 <div className="relative">
                     <img className="w-full h-72 object-cover lg:h-96" src="../../single.jpg" alt="タイプ１" />
                     <div className="absolute bottom-5  right-0 flex items-center justify-center bg-blue-950 opacity-85 h-20 w-52 lg:h-36 lg:w-72 lg:tracking-widest">
-                        <p className="px-12 text-white text-xl  lg:text-2xl">1名様用<br />
-                            ¥9999~</p>
+                        <p className="px-12 text-white text-xl  lg:text-2xl">1名様用/1h<br />
+                            ¥990~</p>
                     </div>
                 </div>
                 <div>
-                    <p className="text-xl text-center pt-5 pb-3">＼こんな方におすすめ！／</p>
+                    <p className="text-xl text-center pt-5 pb-3">＼こんなお客様におすすめ！／</p>
                 </div>
                 <div className="flex justify-around md:justify-center md:gap-16">
-                    <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                    <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center ">
+                        <p className="text-white p-3 text-xs lg:text-sm">集中力を最大限に高めたい方</p>
                     </div>
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-3 text-xs lg:text-sm">クリエイティブ系個人事業主・フリーランスの方</p>
                     </div>
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-3 text-xs lg:text-sm">プライベートな会話やミーティングをしたい方</p>
                     </div>
                 </div>
             </div>
@@ -34,22 +34,22 @@ export default function SpaceType() {
                 <div className="relative">
                     <img className="w-full h-72 object-cover lg:h-96" src="../../afew.jpg" alt="タイプ１" />
                     <div className="absolute bottom-5  right-0 flex items-center justify-center bg-blue-950 opacity-85 h-20 w-52 lg:h-36 lg:w-72 lg:tracking-widest">
-                        <p className="px-12 text-white text-xl  lg:text-2xl">3~4名様用<br />
-                            ¥9999~</p>
+                        <p className="px-12 text-white text-xl  lg:text-2xl">3~4名様用/1h<br />
+                            ¥2990~</p>
                     </div>
                 </div>
                 <div>
-                    <p className="text-xl text-center pt-5 pb-3">＼こんな方におすすめ！／</p>
+                    <p className="text-xl text-center pt-5 pb-3">＼こんなお客様におすすめ！／</p>
                 </div>
                 <div className="flex justify-around md:justify-center md:gap-16">
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-3 text-xs lg:text-sm">コンパクトなオフィスを探しているスタートアップの方</p>
                     </div>
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-4 text-xs lg:text-sm">アイディアを共有したいクリエイターの方々</p>
                     </div>
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-3 text-xs lg:text-sm">チームワークを育みたい！と考えている会社の小規模チーム</p>
                     </div>
                 </div>
             </div>
@@ -57,22 +57,22 @@ export default function SpaceType() {
                 <div className="relative">
                     <img className="w-full h-72 object-cover lg:h-96" src="../../manypeople.jpg" alt="タイプ１" />
                     <div className="absolute bottom-5  right-0 flex items-center justify-center bg-blue-950 opacity-85 h-20 w-52 lg:h-36 lg:w-72 lg:tracking-widest">
-                        <p className="px-12 text-white text-xl  lg:text-2xl">5~10名様用<br />
-                            ¥9999~</p>
+                        <p className="px-12 text-white text-xl  lg:text-2xl">5~10名様用/1h<br />
+                            ¥5800~</p>
                     </div>
                 </div>
                 <div>
-                    <p className="text-xl text-center pt-5 pb-3">＼こんな方におすすめ！／</p>
+                    <p className="text-xl text-center pt-5 pb-3">＼こんなお客様におすすめ！／</p>
                 </div>
                 <div className="flex justify-around md:justify-center md:gap-16">
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-3 text-xs lg:text-sm">大規模プロジェクトの打ち合わせをしたいと考えている方</p>
                     </div>
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-3 text-xs lg:text-sm">プロジェクターや音響など、柔軟に駆使したいイベンターの方</p>
                     </div>
                     <div className="bg-blue-950 h-32 w-32 lg:h-40 lg:w-40 rounded-full flex items-center justify-center">
-                        <p className="text-white p-4 text-xs lg:text-base">テキストテキストテキストテキストテキストテキスト</p>
+                        <p className="text-white p-3 text-xs lg:text-sm">複数チームで意見交換ができる広々とした空間をお探しの方</p>
                     </div>
                 </div>
             </div>
